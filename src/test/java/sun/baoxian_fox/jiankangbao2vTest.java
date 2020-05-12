cff71549ca0dfde114c330f94bea1986cb9b5a8a
Lee-Kwangsoo <sunxp1993@163.com>
2020-05-12 19:03:23 +0800

    修改注释

--- src/test/java/sun/baoxian_fox/jiankangbao2vTest.java
+++ src/test/java/sun/baoxian_fox/jiankangbao2vTest.java
@@ -13,7 +13,7 @@ import static sun.baoxian.actions.mobileList.YYH;
 public class jiankangbao2vTest extends WebCaseBase {
     String url1="https://bxm.ncfimg.com/#/long_insurance/detail?product_code=701911S0061";
     String urlFac="https://h5.zkydib.com/biz/page/info?__t=1589277695&page_id=34&goods_code=G1582798321&channel=xinmeiti&fr=baoxianshangcheng_rexiaobx&product_code=P7020200007#/";
-//    @Test
+    @Test
     public void jiankangbao3() throws Exception{
         jiankangbao2vAction action=new jiankangbao2vAction();
         action.jiankangbao_fox(url1,YYH);
