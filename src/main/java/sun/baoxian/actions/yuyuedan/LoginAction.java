465361aa435d034d5383b1861e0fcd8404196c18
Lee-Kwangsoo <sunxp1993@163.com>
2020-07-21 19:21:41 +0800

    8

--- src/main/java/sun/baoxian/actions/yuyuedan/LoginAction.java
+++ src/main/java/sun/baoxian/actions/yuyuedan/LoginAction.java
@@ -79,7 +79,7 @@ public class LoginAction extends WebCaseBase {
         File image=new File(imageFile);
         ITesseract instance=new Tesseract();
         instance.setTessVariable("user_defined_dpi", "300");
-        instance.setDatapath("src/main/resources/tessdata");
+        instance.setDatapath("src/main/resources/tessdata/");
         instance.setLanguage("eng");
         try {
             content =instance.doOCR(image).replaceAll("\n","");
